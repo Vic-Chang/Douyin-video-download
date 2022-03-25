443a86e82442995e9f3151998c822fef1e64946e
Vic-Chang <s9826111@ulive.pccu.edu.tw>
2022-03-25 17:48:14 +0800

    Get and return real video url

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+import json
 import re
+import requests
+from fastapi import FastAPI
+from urllib.parse import urlparse
 
 app = FastAPI()
 
@@ -10,7 +13,22 @@ async def root():
 
 
 @app.post("/")
-async def download_video(p_str: str):
+async def get_video_url(p_str: str):
+    req = requests.Session()
+    headers = {
+        'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+    }
     regular_patten = r'(?:https:)//.*(?=/)'
-    url = re.search(regular_patten, p_str).group()
-    return {"result": f"{url}"}
+    input_url = re.search(regular_patten, p_str).group()
+    response = req.get(input_url, headers=headers)
+
+    redirect_url = urlparse(response.url).path.rstrip('/')
+    uid = redirect_url.rsplit('/', 1)[1]
+    douyin_url = f'https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids={uid}'
+    video_info = req.get(douyin_url, timeout=5, headers=headers)
+
+    video_info_obj = json.loads(video_info.text)
+    video_url = video_info_obj['item_list'][0]['video']['play_addr']['url_list'][0]
+    video_url = video_url.replace('playwm', 'play')
+
+    return {"result": f"{video_url}"}
